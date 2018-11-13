03b108790c41532d8b88dca85c53f7ecf3f5e53c
lebarquero <lebarquero@gmail.com>
2018-11-12 22:09:42 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/scrum1.jpg'){
+        myImage.setAttribute('src', 'images/scrum2.png');
+    }
+    else {
+        myImage.setAttribute('src', 'images/scrum1.jpg');
+    }
+}
+
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Scrum is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Scrum is cool, ' + storedName;
+}
+
+var myButton = document.querySelector('button');
+
+myButton.onclick = function () {
+    setUserName();
+}
